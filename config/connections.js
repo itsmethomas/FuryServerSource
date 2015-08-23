06963cfcb080dc4d2d75e9dae9e8ad5f78bf6529
itsmethomas <itsmethomas1225@hotmail.com>
2015-08-23 06:46:11 +0300

    dd

--- config/connections.js
+++ config/connections.js
@@ -38,7 +38,7 @@ module.exports.connections = {
     adapter : 'sails-mysql',
     host    : 'localhost',
     user    : 'root',
-    password: '', 
+    password: 'rootroot', 
     database: 'openfire_db'
   },
 
